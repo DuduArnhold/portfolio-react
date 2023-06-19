da27abfd45db5fa9b141c6d9f2c120e0369f65f6
José Eduardo Arnhold <duduarnhold2@gmail.com>
2023-06-19 15:58:50 -0300

    commit da home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import Header from './components/Header/index.jsx';
+import {AiOutlineGithub, AiOutlineLinkedin, } from 'react-icons/ai';
 
+import '../src/app.css'
 
 function App() {
   return (
     <>
-    <header><Header/></header>
+    <header>
+      <Header/>
+    </header>
+
+    <main>
+      <section className='container-home'>
+      <div className='name_icons'>
+        <h1 className='title'>José Eduardo Arnhold</h1>
+        <a href="#">
+          <AiOutlineGithub
+            size='3rem'
+          />
+        </a>
+        <a href="#">
+        <AiOutlineLinkedin
+          size='3rem'
+        />
+        </a>
+        </div>
+        <img  className='img-home' src='./public/images/dev-prod.svg' alt="Undraw illustration de um dev" />
+
+      </section>
+    </main>
       
      
 
