87726cea6e0033e2adaea6e1ef4e78dabf7ec706
José Eduardo Arnhold <duduarnhold2@gmail.com>
2023-06-19 13:59:50 -0300

    first commit com Header component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header/index.jsx';
+
+
+function App() {
+  return (
+    <>
+    <header><Header/></header>
+      
+     
+
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,33 @@
+import './style.css'
+
+
+
+export default function Header(){
+    return (
+        <>
+        <header>
+        <div>
+            <a href="#"><img src="./public/images/Logo.png" alt="Logo site" /></a>
+        </div>
+
+
+        <div className='nav-list'>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">Projetos</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        </header>
+
+        </>
+    )
+}
